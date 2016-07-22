651ace6982f1687e7fad986cad28f3e059868420
longjl <longjl@anbi.tv>
2016-07-22 11:05:06 +0800

    kai ke le

---
+++ projects/api/doc/kaikele.sql
@@ -0,0 +1,92 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50617
+ Source Host           : localhost
+ Source Database       : kaike
+
+ Target Server Type    : MySQL
+ Target Server Version : 50617
+ File Encoding         : utf-8
+
+ Date: 07/22/2016 10:58:18 AM
+*/
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `course`
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL DEFAULT '' COMMENT '课程标题',
+  `course_code` varchar(50) NOT NULL DEFAULT '' COMMENT '课程码',
+  `desc` text COMMENT '课程描述',
+  `cover` varchar(255) NOT NULL DEFAULT '' COMMENT '课程封面',
+  `lesson_number` varchar(10) NOT NULL DEFAULT '0' COMMENT '课节数',
+  `price` int(11) NOT NULL DEFAULT '0' COMMENT '课程价格',
+  `sort` int(11) NOT NULL DEFAULT '0' COMMENT '排序',
+  `status` varchar(10) NOT NULL DEFAULT 'true' COMMENT '状态,true:上架 , false:下架',
+  `url` varchar(255) DEFAULT NULL COMMENT '(音频,视频,课件,程序)地址',
+  `qr_code` varchar(255) DEFAULT NULL COMMENT '课程二维码',
+  `open_date` timestamp NULL DEFAULT NULL COMMENT '开课日期',
+  `create_at` timestamp NULL DEFAULT NULL,
+  `update_up` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `delete_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8 COMMENT='课程信息表';
+
+-- ----------------------------
+--  Table structure for `course_template`
+-- ----------------------------
+DROP TABLE IF EXISTS `course_template`;
+CREATE TABLE `course_template` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `template_id` int(11) NOT NULL DEFAULT '0' COMMENT '模板id',
+  `course_id` int(11) NOT NULL DEFAULT '0' COMMENT '课程id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8 COMMENT='课程模板关联表';
+
+-- ----------------------------
+--  Table structure for `template`
+-- ----------------------------
+DROP TABLE IF EXISTS `template`;
+CREATE TABLE `template` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '模板名称',
+  `desc` text COMMENT '模板描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='课程模板信息表';
+
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '' COMMENT '会员名字',
+  `avatar` varchar(255) NOT NULL DEFAULT '' COMMENT '头像',
+  `mobile` varchar(20) NOT NULL DEFAULT '' COMMENT '手机号',
+  `password` varchar(50) NOT NULL COMMENT '用户密码',
+  `create_at` timestamp NULL DEFAULT NULL,
+  `update_up` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `delete_at` timestamp NULL DEFAULT NULL COMMENT '删除时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户信息表';
+
+-- ----------------------------
+--  Table structure for `user_course`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_course`;
+CREATE TABLE `user_course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL DEFAULT '0' COMMENT '用户id',
+  `course_id` int(11) NOT NULL DEFAULT '0' COMMENT '课程id',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户课程关联表';
+
+SET FOREIGN_KEY_CHECKS = 1;
--- projects/api/doc/readme.md
+++
@@ -1,7 +0,0 @@
-===一些约定  
-task 单个任务对象  
-tasks 多个任务的集合
-tasklist 单个任务列表  
-tasklists 多个任务列表的集合  
-pId projectUuid的简写
-uid userUuid的简写
